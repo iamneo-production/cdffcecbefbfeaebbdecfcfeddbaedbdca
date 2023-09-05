3ed306b923d354c24b2a6a79de859174d8b0f828
Iamneo <techadmin@iamneo.ai>
2023-09-05 10:53:55 +0000

    Backup Commit

---
+++ Demo_Program2/demo1/src/main/java/com/example1/server/Server.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Server {
+    public Connection getconnection() {
+        try {
+
+            
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/appdb", "root", "examly");
+
+            if(conn!=null)
+            {
+                System.out.println("Connected Successfully");
+            }
+            else
+            {
+                System.out.println("Failed to connect");
+            }
+        } catch(Exception e) {
+
+        }
+            return conn;
+
+    }
+}

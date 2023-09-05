be48c81fbf31da861b792b16beb31f0e8168b5ba
Iamneo <techadmin@iamneo.ai>
2023-09-05 11:45:46 +0000

    Backup Commit

--- JAVA_MYSQL_PGM/demo/src/main/java/com/example/Client.java
+++ JAVA_MYSQL_PGM/demo/src/main/java/com/example/Client.java
@@ -1,3 +1,19 @@
+package main.java.com.example;
+
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
-    
+     public static void main(String[] args) {
+        try {
+            // Connect to the server running on localhost at port 12345
+            Socket serverSocket = new Socket("localhost", 12345);
+            System.out.println("Connected to the server!");
+
+            // Close the connection (we're only demonstrating connectivity)
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- JAVA_MYSQL_PGM/demo/src/main/java/com/example/Server.java
+++ JAVA_MYSQL_PGM/demo/src/main/java/com/example/Server.java
@@ -1,3 +1,27 @@
+package main.java.com.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            // Create a server socket and bind it to a port (e.g., 12345)
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Server is running and waiting for clients...");
+
+            while (true) {
+                // Accept incoming client connections
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected from: " + clientSocket.getInetAddress());
+                
+                // Close the client socket (we're only demonstrating connectivity)
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
 }

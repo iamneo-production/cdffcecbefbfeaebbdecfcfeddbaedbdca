fd6919eb0507177afeb46fff362daae27b36e932
Iamneo <techadmin@iamneo.ai>
2023-09-05 11:55:46 +0000

    Backup Commit

--- JAVA_MYSQL_PGM/demo/src/main/java/com/example/App.java
+++ JAVA_MYSQL_PGM/demo/src/main/java/com/example/App.java
@@ -1,13 +1,24 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        // Database connection information
+        String jdbcURL = "mysql://localhost:8080/mysql";
+        String username = "root";
+        String password = "root";
+
+        // Attempt to establish a connection
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Database connection successful!");
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Database connection failed!");
+            e.printStackTrace();
+        }
     }
 }
